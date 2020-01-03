b27b839c35e876adcde9f15d6695d006d547f149
franekwoloch <franciszek.woloch@gmail.com>
2020-01-03 19:57:57 +0100

    added algo for reverse LinkedList

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+
+public class ReverseLinkedList implements Algo {
+
+    LinkedList<Integer> linkedList;
+
+    public ReverseLinkedList() {
+        linkedList=new LinkedList<>();
+
+    }
+
+    @Override
+    public void infoAlgo() {
+        System.out.println("Algo reversing linked list");
+    }
+
+    @Override
+    public void inputAlgo() {
+        System.out.println("Input data for reverse LinkedList:");
+        for (int i=0; i<20; i++){
+            linkedList.add(i);
+            System.out.println(linkedList.get(i));
+        }
+    }
+
+    @Override
+    public void runAlgo() {
+        int size=linkedList.size();
+        if (size!=0){
+            for  (int i=0; i<size;i++){
+                linkedList.addFirst(linkedList.getLast());
+                linkedList.removeLast();
+            }
+        }
+    }
+
+    @Override
+    public void resultsAlgo() {
+        System.out.println("Output data for reverse LinkedList:");
+        for (int i=0; i<linkedList.size(); i++){
+             System.out.println(linkedList.get(i));
+        }
+    }
+}
