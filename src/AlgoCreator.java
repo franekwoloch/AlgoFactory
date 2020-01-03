30dc39680a8bdeebfb1dee0210552383339cc8f4
franekwoloch <franciszek.woloch@gmail.com>
2020-01-03 20:20:28 +0100

    added Algo factory class and Algo creator class

--- src/AlgoCreator.java
+++ src/AlgoCreator.java
@@ -1,5 +1,19 @@
 //implementation of Simply Factory Pattern
 
 public class AlgoCreator {
+    public Algo createAlgo(String type){
+        if (type.equals("FizzBuzz")){
+            return new FizzBuzz();
+        }
+        else if (type.equals("ReverseList")){
+            return new ReverseList();
+        }
+        else if (type.equals("ReverseLinkedList")){
+            return new ReverseLinkedList();
+        }
+        else {
+            throw new IllegalArgumentException("Unknown type "+type );
+        }
     }
+}
 
---
+++ src/AlgoFactory.java
@@ -0,0 +1,15 @@
+public class AlgoFactory {
+    AlgoCreator algoCreator;
+
+    public AlgoFactory(AlgoCreator algoCreator) {
+        this.algoCreator = algoCreator;
+    }
+
+    public void manufactureAlgo(String type){
+        Algo algo=algoCreator.createAlgo(type);
+        algo.infoAlgo();
+        algo.inputAlgo();
+        algo.runAlgo();
+        algo.resultsAlgo();
+    }
+}
